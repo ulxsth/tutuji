a6eb38180bc2dc6e877c81289e4674368c63227f
ulxsth <ulxsth@gmail.com>
2024-03-31 15:29:12 +0900

    fix: ModalBuilder doesn't have setDescription()

--- commands/core/log.js
+++ commands/core/log.js
@@ -11,22 +11,26 @@ module.exports = {
     .setName('log')
     .setDescription('Logs daily record'),
   async execute(interaction) {
-    const modal = new ModalBuilder()
-      .setCustomId("log")
-      .setTitle("📜diary📜")
-      .setDescription("今日の日課の記録をしてね");
+    try {
+      const modal = new ModalBuilder()
+        .setCustomId("log")
+        .setTitle("📜diary📜");
 
-    parameters.forEach((parameter, index) => {
-      let input = new TextInputBuilder()
-        .setCustomId(`log_${index}`)
-        .setLabel(`${parameter.name} (単位：${parameter.unit})`)
-        .setStyle(TextInputStyle.Short);
+      parameters.forEach((parameter, index) => {
+        let input = new TextInputBuilder()
+          .setCustomId(`log_${index}`)
+          .setLabel(`${parameter.name} (単位：${parameter.unit})`)
+          .setStyle(TextInputStyle.Short);
 
-      let actionRow = modal.addActionRow();
-      actionRow.addComponents(input);
-      modal.addActionRow(actionRow);
-    })
+        let actionRow = modal.addActionRow();
+        actionRow.addComponents(input);
+        modal.addActionRow(actionRow);
+      })
 
-    await interaction.showModal(modal);
+      await interaction.showModal(modal);
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({ content: `ERR: ${error}`, ephemeral: true });
+    }
   }
 }
